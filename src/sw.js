bd4ac4419dc7c4ca438232ac5ee000349816e037
Gautam, Ranjeet Kumar <RanjeetKumar_Gautam@intuit.com>
2017-11-11 15:09:03 +0530

    BF

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,7 @@
 
   this.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('v1').then(function(cache) {
+      caches.open('v2').then(function(cache) {
         return cache.addAll([
           '/index.html',
           '/assets/app.js',
@@ -13,22 +13,19 @@
         ]);
       })
     );
-    console.log("All files added in cache");
   });
 
   this.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request).then(function(resp) {
         return resp || fetch(event.request).then(function(response) {
-            return caches.open('v1').then(function(cache) {
+            return caches.open('v2').then(function(cache) {
               cache.put(event.request, response.clone());
-              console.log("Response sent from cache......" + event.request);
               return response;
             });
           });
       })
     );
-    console.log("Added fetch event listener");
   });
 
 })();
--- src/util/fetchData.js
+++ src/util/fetchData.js
@@ -6,27 +6,42 @@ export default function fetchData() {
           title: 'Himalaya Men Intense Oil Clear Lemon Face Wash',
           id: '1',
           desc: `Easy returns & refunds Return products at doorstep and get refund in seconds.
-          100% items delivered Get 150%.`
+          100% items delivered Get 150%. In botany, a fruit is the seed-bearing structure in
+          flowering plants (also known as angiosperms) formed from the ovary after flowering.
+          Fruits are the means by which angiosperms disseminate seeds. In botany, a fruit is
+          the seed-bearing structure in flowering plants (also known as angiosperms) formed
+          from the ovary after flowering. Fruits are the means by which angiosperms disseminate
+          seeds.`
         },
         {
           img: '../styles/assets/2.jpg',
           title: 'Himalaya Men Intense Oil Clear Lemon Face Wash',
           id: '2',
           desc: `Easy returns & refunds Return products at doorstep and get refund in seconds.
-          100% items delivered Get 150%.`
+          100% items delivered Get 150%. In botany, a fruit is the seed-bearing structure in
+          flowering plants (also known as angiosperms) formed from the ovary after flowering.
+          Fruits are the means by which angiosperms disseminate seeds. In botany, a fruit is
+          the seed-bearing structure in flowering plants (also known as angiosperms) formed
+          from the ovary after flowering. Fruits are the means by which angiosperms disseminate
+          seeds.`
         },
         {
           img: '../styles/assets/3.jpg',
           title: 'Himalaya Men Intense Oil Clear Lemon Face Wash',
           id: '3',
           desc: `Easy returns & refunds Return products at doorstep and get refund in seconds.
-          100% items delivered Get 150%.`
+          100% items delivered Get 150%. In botany, a fruit is the seed-bearing structure in
+          flowering plants (also known as angiosperms) formed from the ovary after flowering.
+          Fruits are the means by which angiosperms disseminate seeds. In botany, a fruit is
+          the seed-bearing structure in flowering plants (also known as angiosperms) formed
+          from the ovary after flowering. Fruits are the means by which angiosperms disseminate
+          seeds.`
         }
       ];
 
       setTimeout(() => {
         resolve(data);
-      }, 2000);
+      }, 3000);
 
     })
 }
